48260ba32e7fd796e5e84c778f71befa496e7a9e
carolmota18 <carolinemota78@gmail.com>
2025-05-07 16:06:00 -0300

    producoao

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config():
-    SQLALCHEMY_DATABASE_URI = environ.get('URL_DATABASE_DEV')
+    SQLALCHEMY_DATABASE_URI = environ.get('URL_DATABASE_PROD')
     SQLALCHEMY_DATABASE_TRACK_MOSFICATIONS = False

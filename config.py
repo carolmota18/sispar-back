3f2efa72d1771dddf8e9f1ccc069053160679dbd
carolmota18 <carolinemota78@gmail.com>
2025-05-13 14:26:20 -0300

    ontra alteracao na config

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config():
-    SQLALCHEMY_DATABASE_URI="mysql://root:root@localhost:3306/sispar_t2"
+    SQLALCHEMY_DATABASE_URI= environ.get("URL_DATABASE_PROD")
     SQLALCHEMY_DATABASE_TRACK_MOSFICATIONS = False
